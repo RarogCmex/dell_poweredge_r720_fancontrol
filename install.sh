ef2c86fe85d5a26d62d58fa8592b407a28971569
RarogCmex <denis7774@gmail.com>
2024-09-30 21:18:43 +0500

    My system

--- install.sh
+++ install.sh
@@ -12,14 +12,14 @@ if [ ! -z "$1" ]; then
     TARGETDIR="$1"
 fi
 
-echo "*** Installing packaged dependencies..."
-if [ -x "$(command -v apt-get)" ]; then
-	apt-get update
-	apt-get install -y build-essential python3-virtualenv python3-dev libsensors4-dev ipmitool
-elif [ -x "$(command -v dnf)" ]; then
-	dnf groupinstall -y "Development Tools"
-	dnf install -y python3-virtualenv python3-devel lm_sensors-devel ipmitool
-fi
+# echo "*** Installing packaged dependencies..."
+# if [ -x "$(command -v apt-get)" ]; then
+# 	apt-get update
+# 	apt-get install -y build-essential python3-virtualenv python3-dev libsensors4-dev ipmitool
+# elif [ -x "$(command -v dnf)" ]; then
+# 	dnf groupinstall -y "Development Tools"
+# 	dnf install -y python3-virtualenv python3-devel lm_sensors-devel ipmitool
+# fi
 
 echo "*** Creating folder '$TARGETDIR'..."
 if [ ! -d "$TARGETDIR" ]; then
@@ -47,17 +47,17 @@ fi
 cp fan_control.yaml.example "$TARGETDIR/"
 cp fan_control.py "$TARGETDIR/"
 
-echo "*** Creating, (re)starting and enabling SystemD service..."
-cp fan-control.service /etc/systemd/system/fan-control.service
-sed -i "s#{TARGETDIR}#$TARGETDIR#g" /etc/systemd/system/fan-control.service
-systemctl daemon-reload
-systemctl restart fan-control
-systemctl enable fan-control
+# echo "*** Creating, (re)starting and enabling SystemD service..."
+# cp fan-control.service /etc/systemd/system/fan-control.service
+# sed -i "s#{TARGETDIR}#$TARGETDIR#g" /etc/systemd/system/fan-control.service
+# systemctl daemon-reload
+# systemctl restart fan-control
+# systemctl enable fan-control
 
-echo "*** Waiting for the service to start..."
-sleep 3
+# echo "*** Waiting for the service to start..."
+# sleep 3
 
-echo -e "*** All done! Check the service's output below:\n"
-systemctl status fan-control
+# echo -e "*** All done! Check the service's output below:\n"
+# systemctl status fan-control
 
 set +e
